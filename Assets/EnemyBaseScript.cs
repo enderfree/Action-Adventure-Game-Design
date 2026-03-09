a56b36927539da24fbd317bee0f39eda676618e9
Samuel Fauteux <samuelfauteux7@gmail.com>
2026-03-09 19:08:34 -0400

    Made Base Enemy
    Made Base Enemy, needs to be tested once we have the animations.

---
+++ Assets/EnemyBaseScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EnemyBaseScript : MonoBehaviour, IHittable
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnHit() 
+    {
+        
+    }
+}
